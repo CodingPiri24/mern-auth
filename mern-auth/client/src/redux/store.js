2336ca3a74ccadc3b8d23683e4fce074d0bf7893
CodingPiri24 <pirathis18@gmail.com>
2024-09-18 14:05:34 +0100

    redux persist added

--- mern-auth/client/src/redux/store.js
+++ mern-auth/client/src/redux/store.js
@@ -1,9 +1,25 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import {persistReducer,persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({user:userReducer});
+
+const persistConfig = {
+   ket: 'root',
+   version: 1,
+   storage,
+
+}
+const persistReducer = persistReducer(persistConfig,rootReducer);
 
 export const store = configureStore({
-    reducer: {user:userReducer},
-    middleware: (getDefaultMiddleWare) => getDefaultMiddleWare({
+    reducer: persistReducer,
+    middleware: (getDefaultMiddleWare) =>
+         getDefaultMiddleWare({
         serializableCheck: false,
     }),
 });
+
+
+export const persistor = persistStore(store);
